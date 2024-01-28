709bb33c5acec6b2c4d9c6bc3e18116a061c21ce
shamun-khatri <shamunkhatri@gmail.com>
2024-01-29 00:08:05 +0530

    Made Some Changes For Deployement

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,17 +3,15 @@ from flask_cors import CORS
 from app.routes import employee
 from .extensions import db
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app, origins="https://intellisqr-assignment.vercel.app/")
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
-    db.init_app(app)
-    # app.app_context().push()
-    app.register_blueprint(employee.bp)
 
-    @app.route("/")
-    def start():
-        return "The server is running!"
-    
-    return app
+app = Flask(__name__)
+CORS(app, origins="https://intellisqr-assignment.vercel.app/")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
+db.init_app(app)
+# app.app_context().push()
+app.register_blueprint(employee.bp)
+
+@app.route("/")
+def start():
+     return "The server is running!"
 
