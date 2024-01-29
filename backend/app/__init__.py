0e4b76cc2f6dbfc8f750424cdc411bbaaf9cc899
shamun-khatri <shamunkhatri@gmail.com>
2024-01-29 10:14:30 +0530

    Changed host for backend server

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,7 +5,7 @@ from .extensions import db
 
 
 app = Flask(__name__)
-CORS(app, origins="https://intellisqr-assignment.vercel.app",)
+CORS(app, origins=["https://intellisqr-assignment.vercel.app", "http://localhost:5173"],)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
 db.init_app(app)
 # app.app_context().push()
